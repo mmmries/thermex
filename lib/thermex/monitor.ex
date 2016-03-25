24989b131679fae3ab008475ae08716a89ead4e7
hqmq <michael@riesd.com>
2016-03-25 12:53:53 -0600

    parse out the serial number from the sensor filepath

--- lib/thermex/monitor.ex
+++ lib/thermex/monitor.ex
@@ -8,16 +8,24 @@ defmodule Thermex.Monitor do
   end
 
   def init(sensor_file) do
-    {:ok, sensor_file, @check_interval}
+    serial_number = parse_serial_number_from_filename(sensor_file)
+    {:ok, %{path: sensor_file, serial: serial_number}, @check_interval}
   end
 
-  def handle_info(:timeout, sensor_file) do
-    Logger.debug("temperature update: #{inspect read_temperature(sensor_file)}")
-    {:noreply, sensor_file, @check_interval}
+  def handle_info(:timeout, %{path: path, serial: serial}=state) do
+    Logger.debug("temperature update for #{serial}: #{inspect read_temperature(path)}")
+    {:noreply, state, @check_interval}
   end
-  def handle_info(msg, sensor_file) do
+  def handle_info(msg, state) do
     Logger.error("#{__MODULE__} received an unexpected message", msg)
-    {:noreply, sensor_file, @check_interval}
+    {:noreply, state, @check_interval}
+  end
+
+  defp parse_serial_number_from_filename(sensor_file) do
+    "28-"<>serial = sensor_file
+                    |> Path.dirname
+                    |> Path.basename
+    serial
   end
 
   defp read_temperature(filename) do
@@ -27,7 +35,7 @@ defmodule Thermex.Monitor do
     |> Enum.take(2)
 
     {temperature, _} = parse_temperature(List.first(lines), List.last(lines)) 
-    {filename, temperature / 1000 }
+    temperature / 1000
   end
 
   defp parse_temperature(first_string, second_string) do
