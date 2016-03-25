1dc99f8d81b36f8293fab3dac0425f7ea369aea1
hqmq <michael@riesd.com>
2016-03-24 22:43:38 -0600

    Thermex.Watcher knows how to look for devices

--- lib/thermex/watcher.ex
+++ lib/thermex/watcher.ex
@@ -13,6 +13,7 @@ defmodule Thermex.Watcher do
 
   def handle_info(:check_for_devices, state) do
     Logger.debug "I need to check for devices at #{base_path}"
+    Logger.debug "okay, lets look at #{inspect look_for_sensor_files}"
     {:noreply, state}
   end
   def handle_info(msg, state) do
@@ -22,5 +23,10 @@ defmodule Thermex.Watcher do
 
   defp base_path do
     Application.get_env(:thermex, :base_path)
+  end
+  defp look_for_sensor_files do
+    with {:ok, dirs} <- File.ls(base_path),
+                dirs <- Enum.filter(dirs, &( String.starts_with?(&1, "28-") )),
+    do: Enum.map(dirs, fn(dir)-> Path.join([base_path, dir, "w1_slave"]) end)
   end
 end
