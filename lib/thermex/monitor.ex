d2a506615c13520c8a76a8f44307ebddeecc1549
hqmq <michael@riesd.com>
2016-03-24 23:44:15 -0600

    get temperature readings from each sensor file

--- lib/thermex/monitor.ex
+++ lib/thermex/monitor.ex
@@ -12,11 +12,32 @@ defmodule Thermex.Monitor do
   end
 
   def handle_info(:timeout, sensor_file) do
-    IO.puts "time to check #{sensor_file}"
+    Logger.debug("temperature update: #{inspect read_temperature(sensor_file)}")
     {:noreply, sensor_file, @check_interval}
   end
   def handle_info(msg, sensor_file) do
     Logger.error("#{__MODULE__} received an unexpected message", msg)
     {:noreply, sensor_file, @check_interval}
+  end
+
+  defp read_temperature(filename) do
+    lines = filename
+    |> File.stream!
+    |> Stream.map(&String.strip/1)
+    |> Enum.take(2)
+
+    {temperature, _} = parse_temperature(List.first(lines), List.last(lines)) 
+    {filename, temperature / 1000 }
+  end
+
+  defp parse_temperature(first_string, second_string) do
+    cond do 
+      String.ends_with?(first_string, "YES") ->
+        String.split(second_string, "=") 
+        |> List.last 
+        |> Integer.parse 
+      true ->
+        nil
+    end
   end
 end
